9a263a4e8c905bed4407c2abda2bad9ce103399a
Gizem <gizem.ates@hvl.no>
2023-06-14 12:31:21 +0200

    modularized ez_copy.sh

--- ui/ez_copy.sh
+++ ui/ez_copy.sh
@@ -1,28 +1,20 @@
-#!/bin/sh
-file1="main_window"
-file2="new_patient"
-file3="progress_window"
-file4="first_patient_selection"
-file5="add_to_existing_patient"
-input_suffix=".ui"
-output_suffix=".py"
+#!/bin/bash
+# Input Files Array
+input_files=("main_window.ui" "new_patient.ui" "threshold_selection_window.ui" "first_patient_selection.ui" "add_to_existing_patient.ui" "real_time_measurement.ui")
 
+# Paths
+ui_dir="/home/gizem/catkin_ws/src/appirh_project_helse_frde/ui/"
+output_dir="/home/gizem/catkin_ws/src/appirh_project_helse_frde/src/Classes/"
 
-input_file1=$file1$input_suffix
-input_file2=$file2$input_suffix
-input_file3=$file3$input_suffix
-input_file4=$file4$input_suffix
-input_file5=$file5$input_suffix
+# Output Files Array (derived from Input Files)
+output_files=()
 
-output_file1=$file1$output_suffix
-output_file2=$file2$output_suffix
-output_file3=$file3$output_suffix
-output_file4=$file4$output_suffix
-output_file5=$file5$output_suffix
+# Loop through the input files array and generate the corresponding output files
+for input_file in "${input_files[@]}"
+do
+    # Derive the output file name from the input file name
+    output_file="${input_file%.ui}.py"
+    output_files+=("$output_file")
 
-
-pyuic5 -x /home/gizem/catkin_ws/src/appirh_project_helse_frde/ui/$input_file1 -o /home/gizem/catkin_ws/src/appirh_project_helse_frde/src/Classes/$output_file1
-pyuic5 -x /home/gizem/catkin_ws/src/appirh_project_helse_frde/ui/$input_file2 -o /home/gizem/catkin_ws/src/appirh_project_helse_frde/src/Classes/$output_file2
-pyuic5 -x /home/gizem/catkin_ws/src/appirh_project_helse_frde/ui/$input_file3 -o /home/gizem/catkin_ws/src/appirh_project_helse_frde/src/Classes/$output_file3
-pyuic5 -x /home/gizem/catkin_ws/src/appirh_project_helse_frde/ui/$input_file4 -o /home/gizem/catkin_ws/src/appirh_project_helse_frde/src/Classes/$output_file4
-pyuic5 -x /home/gizem/catkin_ws/src/appirh_project_helse_frde/ui/$input_file5 -o /home/gizem/catkin_ws/src/appirh_project_helse_frde/src/Classes/$output_file5
+    pyuic5 -x "$ui_dir$input_file" -o "$output_dir$output_file"
+done
